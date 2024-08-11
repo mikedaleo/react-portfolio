d7cf93e3b937d7e9b5e5464190172b88de853caa
mikedaleo <mikedaleo@outlook.com>
2024-08-10 21:22:14 -0400

    initial code setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Navigation from './Navigation';
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <h1>Michael Daleo's Portfolio</h1>
+                <Navigation></Navigation>
+            </header>
+        </div>
+    )
+
+
+}
