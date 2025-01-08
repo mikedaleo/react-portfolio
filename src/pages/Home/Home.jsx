3dacb4314a4b46fe921ca878e0043c70cd9b01b7
mikedaleo <mikedaleo@outlook.com>
2025-01-08 13:49:48 -0500

    finished about page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import './Home.css' 
 import './Home';
-
+import pressStart from '../../assets/press_start_cropped.gif'
+import { Link } from 'react-router-dom'
+ 
 export default function Home() {
     return (
         <div className='home-div'>
            <h1>MICHAEL DALEO</h1>
             <h2>&lt;&gt;WEB DEVELOPER&lt;/&gt;</h2>
-            <a href="/about" id='startBtn'>PRESS START</a>
+            <Link to={'/about'}><img src={pressStart} alt="" /></Link>
 
             <footer>© CREATED BY MICHAEL DALEO</footer>
         </div>
